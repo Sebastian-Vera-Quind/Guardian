824fa67056296a9fe524358235330d29295f2132
Ru-kko <verase2003.1@gmail.com>
2026-06-24 17:03:37 -0500

    feat: implement colone path node

--- src/infra/adapters/workflow/nodes/__init__.py
+++ src/infra/adapters/workflow/nodes/__init__.py
@@ -1,3 +1,4 @@
 from src.infra.adapters.workflow.nodes.loader import node_loader_task
+from src.infra.adapters.workflow.nodes.clone_path import node_clone_task
 
-__all__ = ["node_loader_task"]
+__all__ = ["node_loader_task", "node_clone_task"]
