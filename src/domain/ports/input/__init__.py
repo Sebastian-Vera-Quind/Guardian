232ebcea2fcee1f28d272f935fc565a96e705e29
Ru-kko <verase2003.1@gmail.com>
2026-07-01 15:22:50 -0500

    feat: add PromptBuilder contract for agent prompt construction and sanitization

--- src/domain/ports/input/__init__.py
+++ src/domain/ports/input/__init__.py
@@ -1,5 +1,11 @@
 from .workflow_executor import WorkflowExecutor
 from .clone.clone_service import CloneService
 from .context.context_retrieval import ContextRetriever
+from .prompt import PromptBuilder
 
-__all__ = ["WorkflowExecutor", "CloneService", "ContextRetriever"]
+__all__ = [
+    "WorkflowExecutor",
+    "CloneService",
+    "ContextRetriever",
+    "PromptBuilder",
+]
---
+++ src/domain/ports/input/prompt/__init__.py
@@ -0,0 +1,3 @@
+from .prompt_builder import PromptBuilder, PromptAttributes
+
+__all__ = ["PromptBuilder", "PromptAttributes"]
